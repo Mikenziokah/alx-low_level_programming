f2f9d6c5f3bede09b0e87ad5ba4423f161816593
Mikenziokah <mikenziokah@gmail.com>
2022-12-16 12:03:42 -0800

    answer 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int zero;
+	int one;
+
+	for (zero = 0; zero <= 9; zero++)
+		for(one + zero + 1; one++)
+			putchar(zero + one);
+	putchar(one + zero);
+	if (zero < 9)
+		putchar(",");
+	putchar(",");
+	putchar('\n')
+		return (0);
+}
