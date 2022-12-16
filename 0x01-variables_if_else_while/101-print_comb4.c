c6263220daabc3b98deac6caa1ef000cc0adcc49
Mikenziokah <mikenziokah@gmail.com>
2022-12-16 12:21:01 -0800

    answer 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++)
+		for (b =0; b < 10; b++)
+			if (b > a)
+				for (c = 0; c < 10; c++)
+					if (c > b)
+						putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(' ');
+	putchar('\n');
+		return (0);
+}
+	
