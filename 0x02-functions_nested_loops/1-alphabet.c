8685b0251e4b71adc973338854c89b825eabdec5
Mikenziokah <mikenziokah@gmail.com>
2022-12-18 02:04:59 -0800

    answer 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "MAIN.H"
+
+/**
+ * main - Entry point
+ * Return Always 0(success)
+ */
+int main(void)
+	void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+		_putchar(c);
+			c++;
+			_putchar('\n');
+			return (0);
+}
