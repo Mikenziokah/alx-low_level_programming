ed78c9bf43e9aaf9d7a25433376fa2c94c731abd
Mikenziokah <mikenziokah@gmail.com>
2023-03-16 13:25:42 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main entry point
+ *
+ * Return:Always (success)
+ */
+int main(void)
+	puts("\n "Programming is like building a multilingual puzzle");
+{
+return 0;
+
+}
