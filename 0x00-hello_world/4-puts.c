529ab2ffbd772c13049ceb60466674a367619a9a
Mikenziokah <mikenziokah@gmail.com>
2023-03-16 13:28:55 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Return:Always (success)
  */
 int main(void)
-	puts("\n "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 {
 return 0;
 
