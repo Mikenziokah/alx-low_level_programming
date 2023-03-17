43878cd0b5a417fd5df9681eaca0e81981f86ef2
Mikenziokah <mikenziokah@gmail.com>
2023-03-17 14:26:20 -0700

    for loops alphabetic

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,10 @@
 
 int main(void)
 {
-	char i:
-		for (i = 'a' ; i <= 'z' ; i++)
-			putchar(i);
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
 	for (i = 'A' ; i <= ; 'Z'; i++)
 		putchar(i);
 	putchar('\n');
