3e45abedf941bc4f02896726f8956d6242418225
Mikenziokah <mikenziokah@gmail.com>
2023-03-19 03:42:07 -0700

    alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always o (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'z'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
