9a75cc7618a1575afa9d32d3ec87c433a46ecc41
Mikenziokah <mikenziokah@gmail.com>
2023-03-24 13:12:35 -0700

    diagnol

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,19 +12,20 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
-	}
+	{
 	for (a = 0; a < n; a++)
 	{
-		for (b = 0; b < n; b++)
-		{
-			if (b == a)
-				_putchar('\\');
-			else if (b < a)
-				_putchar(' ');
-		}
-		_putchar('\n');
+	for (b = 0; b < n; b++)
+	{
+	if (b == a)
+	_putchar('\\');
+	else if (b < a)
+	_putchar(' ');
+	}
+	_putchar('\n');
+	}
 	}
 }
