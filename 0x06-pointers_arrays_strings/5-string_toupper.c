cb0c5149eb6bb2e3ed491fdf2c907c8660eba779
Mikenziokah <mikenziokah@gmail.com>
2023-03-30 07:21:22 -0700

    touppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,14 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; i != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
-			str++;
 		}
+		i++;
 	}
 	return (str);
 }
