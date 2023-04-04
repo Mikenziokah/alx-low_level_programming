0892baa3f7516b2ea8658495ef9936702192d7b6
Mikenziokah <mikenziokah@gmail.com>
2023-04-04 12:33:35 -0700

    chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @needle: string to be found in
+ * @haystack:  string to be searched
+ * Return: always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int j = 0;
+
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		if (haystack[j] == needle[j])
+		{
+			j++;
+		}
+		while (
+	
+
