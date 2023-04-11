bfd6c38f1d268655fe5f2cb77b6f903483615ef3
Mikenziokah <mikenziokah@gmail.com>
2023-04-11 12:15:12 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	j = malloc(sizeof(char) * size);
 	if (size == 0 && j == 0)
-		return (0);
+		return (NULL);
 	for (i = 0; i < size; i++)
 		j[i] = c;
 	return (j);
