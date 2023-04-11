ea0a8fff3a7e3dfa995967a058e6b40330e844d1
Mikenziokah <mikenziokah@gmail.com>
2023-04-11 13:06:17 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated memory
+ * @str: string which dublicates another string
+ * Return: a string
+ */
+
+char *_strdup(char *str)
+{
+	int i, j = 0;
+	char k;
+
+	if (str == NULL)
+		return (NULL);
+	k = malloc(sizeof(char) * (i = 1));
+	return (k);
+}
