aca810d0a59ef4def29f6ec7c23243bb84c07038
Mikenziokah <mikenziokah@gmail.com>
2023-04-13 12:14:54 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -15,5 +14,5 @@ void *malloc_checked(unsigned int b)
 	x = malloc(b);
 	exit(98);
 	if (x == NULL)
-		return (x);
+		free(x);
 }
