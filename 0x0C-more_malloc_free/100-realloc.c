946ef9ef3dedb81ed01845ec338f6c057ce3df3d
Mikenziokah <mikenziokah@gmail.com>
2023-04-14 12:19:47 -0700

    advanced 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,6 +19,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 		ptr = malloc(new_size);
+	if (new_size > old_size)
+
 	if (new_size == old_size)
 		return (ptr);
 	ptr = malloc(new_size);
