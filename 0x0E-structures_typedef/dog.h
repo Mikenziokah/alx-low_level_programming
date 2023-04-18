b7a40a3f23f4dd508ea9691783e7b1727425c490
Mikenziokah <mikenziokah@gmail.com>
2023-04-18 13:11:04 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - new type of
+ * @name: dogs identity
+ * @age: years of the dog
+ * @owner: dogs owner
+ * Return: dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "main."
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
+void int_dog(struct dog *d, char *name, float age, char *owner);
+#endif
