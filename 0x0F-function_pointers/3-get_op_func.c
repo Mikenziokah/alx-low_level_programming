b2c8ba226f18fd174fcd4456a7d4e81e8b2fd9ea
Mikenziokah <mikenziokah@gmail.com>
2023-04-20 08:46:58 -0700

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -14,15 +14,11 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
-	};
+		{NULL},
+		};
 	int i = 0;
 
-	while (i < 0)
-	{
-		if (strcmp(s, ops[i].op == 0)
-				return (ops[i].f);
-				i++;
-				}
-				return (0);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+	return (ops[i]);
 }
