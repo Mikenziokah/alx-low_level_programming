d86f6928871fc7da015e6983f9c32bf408a569ec
Mikenziokah <mikenziokah@gmail.com>
2023-04-21 13:08:06 -0700

    advanced

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdlb.h>
+#include <stdio.h>
+/**
+ * main - prints opcodes of its own main function
+ * @argc: counter
+ * @argv: vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	char *i;
+	int bytes, x;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	i = (char *)main;
+	for (x = 0; x < bytes; x++)
+	{
+		if (x == bytes - 1)
+		{
+		printf("%02hhx\n", i[x]);
+		break;
+	}
+	printf("02hhx", i[x]);
+	}
+	return (0);
+}
