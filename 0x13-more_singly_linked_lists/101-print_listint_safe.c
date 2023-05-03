550e8abc959210eb4ada10a7730cefa82b65cd89
Mikenziokah <mikenziokah@gmail.com>
2023-05-03 13:52:16 -0700

    advanced

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * reverse_listint - reverses a listint_t linked list
+ * @head: pointer
+ * Return: number of nodes
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	size_t x, y =0;
+	const listint_t **list =NULL
+
+		while (head != NULL)
+		{
+			for (x = 0; x < y; x++)
+			{
+				if (head == list[x])
+					printf("-> [%p] %d\n", (void *)head, head_>n);
+					free(list);
+					return (y);
+			}
+		}
+}
