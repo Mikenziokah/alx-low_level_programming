18306d8b71420786a318bf8177019195ea43600c
Mikenziokah <mikenziokah@gmail.com>
2023-06-09 12:01:12 -0700

    libmy

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper characters
+ * @c: the letter to be checked
+ * Return: 1 for uppercase 0 for another
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
+
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - returns char
+ * @s:input
+ * Return: always 0
+ */
+
+int _atoi(char *s)
+{
+	return (0);
+}
+
