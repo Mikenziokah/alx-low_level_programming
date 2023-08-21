0affd4247099eef448a97699f38cd5fa38bdae0b
Mikenziokah <mikenziokah@gmail.com>
2023-08-21 12:43:58 -0700

    libmiso

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @n: numberof bytes
+ * @src: copied from here
+ * @dest: copied to here
+ * Return: destination memory point
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates charcters in astring
+ * @c: charcter to be searched
+ * @s: string input
+ * Return: always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
+
