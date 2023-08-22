4edfcdd40c655010476a747dba914d55fd4e2b21
Mikenziokah <mikenziokah@gmail.com>
2023-08-22 13:06:07 -0700

    advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+
+/**
+ * add - my addition
+ * @a: number
+ * @b: number
+ * Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtraction
+ * @a: number
+ * @b: number
+ * Return: Subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: number
+ * Return: Multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Division
+ * @a: number
+ * @b: number
+ * Return: Division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - retrun modulo
+ * @a: number
+ * @b: number
+ * Return: modulo
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
